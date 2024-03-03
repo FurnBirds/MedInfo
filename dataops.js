72bf3b197411f23e3d2c6d8ed73aa3aace5a52ef
Steven <furnbirds@gmail.com>
2024-03-03 20:37:19 +0800

    built fetch funtion to populate patient data table, further styled pt data card and pt data table by defining more class in elements tags

--- dataops.js
+++ dataops.js
@@ -1,11 +1,32 @@
-function retrieveProfiles() {
-    fetch('http://localhost:3000/ptProfile')
-    .then(package => package.json)
-    // .then(ptProfiles => ptProfiles.array.forEach(element => {
 
-        
-    // });)
+function constructPtDataRow(ptData){
     
+    let ptDataRow = document.createElement('tr')
+    ptDataRow.className = 'row'
+    ptDataRow.innerHTML = `
+    <td class = "ptID">${ptData.id}</td>
+    <td class = "names">${ptData.surname}</td>
+    <td class = "names">${ptData.firstname}</td>
+    <td class = "ptGender">${ptData.gender ?? ""}</td>
+    <td class = "ptDOB">${ptData.dob ?? ""}</td>
+    <td class = "ptBP">${ptData.systolicBP ?? ""} /  ${ptData.diastolicBP ?? ""}</td>
+
     
+    `
+    document.getElementById('profiletable').appendChild(ptDataRow)
 }
-{}
+
+function getPtProfiles() {
+    fetch('http://localhost:3000/ptProfile')
+    .then(package => package.json())
+    .then(ptListing => ptListing.forEach(ptData => {constructPtDataRow(ptData)
+        
+    })
+    )
+};
+
+function resetPtProfileList(){
+    getPtProfiles()
+}
+
+resetPtProfileList()
