0de282fc0847ea0901876f446f4d968bf956855d
Steven <furnbirds@gmail.com>
2024-03-03 16:11:58 +0800

    changed json file name

--- dataops.js
+++ dataops.js
@@ -1,10 +1,10 @@
 function retrieveProfiles() {
     fetch('http://localhost:3000/ptProfile')
     .then(package => package.json)
-    .then(ptProfiles => ptProfiles.array.forEach(element => {
+    // .then(ptProfiles => ptProfiles.array.forEach(element => {
+
         
-        
-    });)
+    // });)
     
     
 }
