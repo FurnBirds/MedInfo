73b343ecadf9d1366c4d5fe4b4d9dc83e6ee7054
Steven <furnbirds@gmail.com>
2024-03-03 15:34:39 +0800

    First commit. Continue from phase 0 project. Adjusted styling to have patient profiles table section on left container, editable selectable patient profile on right.

---
+++ dataops.js
@@ -0,0 +1,11 @@
+function retrieveProfiles() {
+    fetch('http://localhost:3000/ptProfile')
+    .then(package => package.json)
+    .then(ptProfiles => ptProfiles.array.forEach(element => {
+        
+        
+    });)
+    
+    
+}
+{}
