9c946cc6e7c33fdcc2b72b258a94966c828933d9
Steven <furnbirds@gmail.com>
2024-03-04 16:39:24 +0800

    created function to translate values for gender to text, on click of row, now names and gender populates into fields of pt data card.

--- dataops.js
+++ dataops.js
@@ -7,11 +7,13 @@ function constructPtDataRow(ptData){
     let ptDataRow = document.createElement('tr')
     ptDataRow.className = 'row'
     ptDataRow.dataID = ptData.id
+    let ptGender = genderTranslation(ptData.gender)
+
     ptDataRow.innerHTML = `
     <td class = "ptID" id = "ptID">${ptData.id}</td>
     <td class = "names" id = "ptSurname">${ptData.surname}</td>
     <td class = "names" id = "ptFirstname">${ptData.firstname}</td>
-    <td class = "ptGender">${ptData.gender ?? ""}</td>
+    <td class = "ptGender">${ptGender}</td>
     <td class = "ptDOB">${ptData.dob ?? ""}</td>
     <td class = "ptOccupation">${ptData.occupation ?? ""}</td>
     <td class = "ptBP">${ptData.systolicBP ?? ""} /  ${ptData.diastolicBP ?? ""}</td>
@@ -22,9 +24,44 @@ function constructPtDataRow(ptData){
     //console.log(ptData.id)
 }
 
+function genderTranslation(genderVal){
+    let ptGender
+    switch(genderVal){
+        case 1:
+            ptGender = "Male";
+            break;
+        case 0:
+            ptGender = "Female";
+            break;
+        case 9:
+            ptGender = "Other";
+            break;
+        default:
+            ptGender = "";
+            break;
+    }
+    return ptGender;
+}
 
 function rowClick() {
-    console.log(this.querySelector('.ptGender').innerText)
+    document.getElementById('fname').value = this.querySelector('#ptFirstname').innerText;
+    document.getElementById('lname').value = this.querySelector('#ptSurname').innerText;
+    
+    
+    switch(this.querySelector('.ptGender').innerText){
+        case "Male":
+            document.getElementById('Male').checked = true;
+            break;
+        case "Female":
+            document.getElementById('Female').checked = true;
+            break;
+        case "Other":
+            document.getElementById('Other').checked = true;
+            break;
+        default:
+            break;
+    }
+    
     
     
 }
